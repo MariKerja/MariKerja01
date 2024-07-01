6e0c161b769bb6434c9c7d28a1195e0ff92a7576
MuniirAhmadi <139943294+MuniirAhmadi@users.noreply.github.com>
2024-07-01 19:39:31 +0800

    try

--- server/index.js
+++ server/index.js
@@ -14,10 +14,31 @@ mongoose
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/", require("./routes/authRoutes"));
+
+
+const Job = mongoose.model('Job', new mongoose.Schema({
+  jobname: String,
+  address: String,
+  description: String,
+  requirement: String,
+  status: String,
+  state: String,
+  district: String,
+  startSalary: String,
+  endSalary: String
+}));
+
+app.post('/postjob', async (req, res) => {
+  const job = new Job(req.body);
+  try {
+    await job.save();
+    res.status(201).send(job);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
